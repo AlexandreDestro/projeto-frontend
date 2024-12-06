6bf4ff46d9f7207dd57d1c4474ee1e2fdc11112b
William Manoel Bitencourt Mesquita <williammbitencourtmesquita@gmail.com>
2024-12-06 14:26:07 -0300

    Fixed Login route and its business logic

--- projeto-frontend/src/layouts/Header/Header.tsx
+++ projeto-frontend/src/layouts/Header/Header.tsx
@@ -1,23 +1,30 @@
+import { useContext } from "react";
+import { LoginContext } from '../../stores/LoginContext';
 import { Button, ButtonGroup } from '@mui/material';
 import { Outlet, Link} from 'react-router-dom';
+
+//import image from '../../assets/user-login-icon.png'
 
 import './Header.css'
 
 export default function Header(){
+    const { user } = useContext(LoginContext);
 
     return(
         <>
         <div className="container">
             <div className="side_bar">
-                    <ButtonGroup  orientation="vertical" variant="text" aria-label="Basic button group" sx={{'& .MuiButton-root': {
-                    color: "#4fc54b", borderColor: "#22b43b", padding: "20px" }}}>
-                        <Button component={Link} to="/">Dashboard</Button>
-                        <Button component={Link} to="/DonationRegistration">Registro de doações</Button>
-                        <Button component={Link} to="/DonationView">Visualização de Doações</Button>
-                        {/* <Button component={Link} to="/ItemDetails">Detalhes do Item</Button> */}
-                        <Button component={Link} to="/Notifications">Notificações</Button>
-                        <Button component={Link} to="/Contacts">Contatos</Button>
-                    </ButtonGroup>        
+                <div id='top-content'>Welcome, {user.username}.</div>
+                <div id='middle-content'></div>
+                <ButtonGroup  orientation="vertical" variant="text" aria-label="Basic button group" sx={{'& .MuiButton-root': {
+                color: "#4fc54b", borderColor: "#22b43b", padding: "20px"}}}>
+                    <Button component={Link} to="/">Dashboard</Button>
+                    <Button component={Link} to="/DonationRegistration">Registro de doações</Button>
+                    <Button component={Link} to="/DonationView">Visualização de Doações</Button>
+                    {/* <Button component={Link} to="/ItemDetails">Detalhes do Item</Button> */}
+                    <Button component={Link} to="/Notifications">Notificações</Button>
+                    <Button component={Link} to="/Contacts">Contatos</Button>
+                </ButtonGroup>        
             </div>
             <main className="main_content">  
                 <Outlet />
--- projeto-frontend/src/pages/Login/Login.tsx
+++ projeto-frontend/src/pages/Login/Login.tsx
@@ -1,59 +1,75 @@
 import React, { useState, useContext } from "react";
 import { LoginContext } from "../../stores/LoginContext";
-import { testLogin } from "../../actions/Login";
 
 import "./Login.css";
 import { Box, Button, Container, TextField, Typography } from "@mui/material";
 
+import users from '../../data/user.json';
+
+type User = {
+  id: number;
+  username: string;
+  password: string;
+  email: string;
+}
+
+const emptyUser: User = {
+  id: 0,
+  username: '',
+  password: '',
+  email: '',
+}
+
+
 function Login() {
   const { login } = useContext(LoginContext);
-
-  const [username, setUsername] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
   const [error, setError] = useState<string>('');
+  const [userDto, setUserDto] = useState<User>(emptyUser);
 
-  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target;
+    setUserDto((prev) => ({
+      ...prev,
+      [name]: value, // Dynamically updates the state based on the `name` attribute
+    }));
+  };
+
+  const submitForm = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-
-    const username = formData.get("username") as string;
-    const password = formData.get("password") as string;
-
-    if (username === '' || password === '' ) { 
+    if (userDto.username === '' || userDto.password === '' ) {
+      setUserDto(emptyUser); 
       setError('Ambos os campos são obrigatórios!');
     } else {
-      setError('');
-    }
-
-    try {
-      if (username !== '' || password !== '') { 
-        const userData = await testLogin(username, password);
-        console.log(userData);    
-        login(userData);
+      const matchedUser: User | undefined = users.find(
+        (user) => userDto.username === user.username && userDto.password === user.password
+      );
+  
+      if(matchedUser){
+        login(matchedUser);
+      }else{
+        setUserDto(emptyUser);
+        setError('Verifique seu usuário e senha!');
       }
-    } catch {
-      setError("Não foi possível fazer login, verifique seu usuário e senha.");
     }
   };
 
   return (
-    <>
-      <div className="login_center">
+    <div className="login_center">
       <Container maxWidth="xs">
-      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: 3 }}>
-        <Typography variant="h5" gutterBottom> Login </Typography>
-        <form onSubmit={handleLogin} style={{ width: '100%' }}>
-          <TextField label="Username" variant="outlined" fullWidth margin="normal" name='username' value={username}
-            onChange={(e) => setUsername(e.target.value)} />
-          <TextField label="Password" type="password" variant="outlined" fullWidth margin="normal" value={password} name='password'
-            onChange={(e) => setPassword(e.target.value)} />
-          {error && <Typography color="error">{error}</Typography>}
-          <Button type="submit" variant="contained" color="primary" fullWidth sx={{ marginTop: 2 }}> Login </Button>
-        </form>
-      </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: 3 }}>
+          <Typography variant="h5" gutterBottom> Login </Typography>
+          <form onSubmit={submitForm} style={{ width: '100%' }}>
+            <TextField label="Username" variant="outlined" fullWidth margin="normal" name='username' value={userDto.username}
+              onChange={handleInputChange} />
+            <TextField label="Password" type="password" variant="outlined" fullWidth margin="normal" name='password' value={userDto.password}
+              onChange={handleInputChange} />
+            {error && <Typography color="error">{error}</Typography>}
+            <Button type="submit" variant="contained" color="primary" fullWidth sx={{ marginTop: 2 }}> Login </Button>
+          </form>
+        </Box>
       </Container>
-      </div>
-    </>
+    </div> 
   );
 }
 
